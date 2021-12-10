070198f895f55425b841ac7786f91c1c31fccdbd
Usman Suleiman <usmansbk@gmail.com>
2021-12-10 19:20:38 +0100

    Create user model

---
+++ src/db/models/user.js
@@ -0,0 +1,55 @@
+import { Model } from "sequelize";
+
+export default (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init(
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      fullName: {
+        type: DataTypes.VIRTUAL,
+        get() {
+          return [this.firstName, this.lastName].join(" ");
+        },
+        set() {
+          throw new Error("Do not try to set the `fullName` value!");
+        },
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      phoneNumber: {
+        type: DataTypes.STRING,
+      },
+      locale: {
+        type: DataTypes.STRING,
+        defaultValue: "en",
+      },
+    },
+    {
+      sequelize,
+      modelName: "User",
+    }
+  );
+  return User;
+};
