f1f8c567b3b1946e6e487761de2220bb325c5b29
Usman Suleiman <usmansbk@gmail.com>
2021-12-10 12:06:57 +0100

    Setup babel

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-const App = () => console.log("Hello World!");
+const Asynch = async () => await Promise.resolve("Hello");
+
+const App = async () => {
+  await Asynch();
+  console.log("Hello");
+};
+
+App();
