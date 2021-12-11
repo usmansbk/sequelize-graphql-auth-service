25367699b85cfe88ac245347e246cc7733aae0fc
Usman Suleiman <usmansbk@gmail.com>
2021-12-11 10:25:59 +0100

    Set test database name

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,13 @@
 require("dotenv").config();
 
-const { DB_USERNAME, DB_PASSWORD, DB_NAME, DB_HOST, DB_DIALECT } = process.env;
+const {
+  DB_USERNAME,
+  DB_PASSWORD,
+  DB_NAME_DEV,
+  DB_NAME_TEST,
+  DB_HOST,
+  DB_DIALECT,
+} = process.env;
 
 const dialect = DB_DIALECT;
 const host = DB_HOST;
@@ -9,14 +16,14 @@ module.exports = {
   development: {
     username: DB_USERNAME,
     password: DB_PASSWORD,
-    database: DB_NAME,
+    database: DB_NAME_DEV,
     host,
     dialect,
   },
   test: {
     username: DB_USERNAME,
     password: DB_PASSWORD,
-    database: DB_NAME,
+    database: DB_NAME_TEST,
     host,
     dialect,
   },
