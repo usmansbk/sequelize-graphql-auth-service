119d1f96924a070d6cf636bd12c94e465007637b
Usman Suleiman <usmansbk@gmail.com>
2021-12-11 15:46:01 +0100

    Add resolver stub

---
+++ src/graphql/resolvers/auth.js
@@ -0,0 +1,16 @@
+export default {
+  Mutation: {
+    loginWithEmailAndPassword() {},
+    requestOTP() {},
+    loginWithOTP() {},
+    socialLogin() {},
+    registerWithEmailAndPassword() {},
+    resendConfirmationEmail() {},
+    confirmEmail() {},
+    requestPasswordReset() {},
+    resetPassword() {},
+    requestDeleteAccount() {},
+    deleteAccount() {},
+    logout() {},
+  },
+};
