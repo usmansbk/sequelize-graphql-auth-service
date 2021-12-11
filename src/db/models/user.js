792d81304cbbd48a56f66ce49590856b321a2262
Usman Suleiman <usmansbk@gmail.com>
2021-12-11 13:43:16 +0100

    Test validation

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -31,13 +31,13 @@ export default (sequelize, DataTypes) => {
         validate: {
           max: {
             args: 100,
-            msg: "name_too_long",
+            msg: "nameTooLong",
           },
           notNull: {
-            msg: "firstname_required",
+            msg: "nameRequired",
           },
           notEmpty: {
-            msg: "empty_firstname",
+            msg: "emptyName",
           },
         },
       },
@@ -47,13 +47,13 @@ export default (sequelize, DataTypes) => {
         validate: {
           max: {
             args: 100,
-            msg: "name_too_long",
+            msg: "nameTooLong",
           },
           notNull: {
-            msg: "lastname_required",
+            msg: "nameRequired",
           },
           notEmpty: {
-            msg: "empty_lastname",
+            msg: "emptyName",
           },
         },
       },
@@ -72,23 +72,31 @@ export default (sequelize, DataTypes) => {
         unique: true,
         validate: {
           isEmail: {
-            msg: "invalid_email",
+            msg: "invalidEmail",
           },
         },
       },
       phoneNumber: {
         type: DataTypes.STRING,
         unique: true,
+        validate: {
+          isNull: {
+            msg: "invalidPhoneNumber",
+          },
+          notEmpty: {
+            msg: "invalidPhoneNumber",
+          },
+        },
       },
       password: {
         type: DataTypes.STRING,
         allowNull: false,
         validate: {
           notEmpty: {
-            msg: "invalid_password",
+            msg: "invalidPassword",
           },
           notNull: {
-            msg: "invalid_password",
+            msg: "invalidPassword",
           },
         },
       },
@@ -97,7 +105,7 @@ export default (sequelize, DataTypes) => {
         defaultValue: "en",
         validate: {
           isAlpha: {
-            msg: "invalid_locale",
+            msg: "invalidLocale",
           },
         },
       },
--- test/models/user.spec.js
+++ test/models/user.spec.js
@@ -1,9 +1,63 @@
 import db from "~db/models";
+import faker from "faker";
 
-const { User } = db.sequelize;
+const { User, sequelize } = db;
+
+const mockUser = {
+  firstName: faker.name.firstName(),
+  lastName: faker.name.lastName(),
+  email: faker.internet.email(),
+  password: faker.internet.password(6),
+  phoneNumber: faker.phone.phoneNumber(),
+  locale: faker.address.countryCode(),
+};
 
 describe("User model", () => {
-  test("2 + 2", () => {
-    expect(2 + 2).toBe(4);
+  beforeAll(async () => {
+    await sequelize.authenticate();
+  });
+
+  afterAll(async () => {
+    User.drop();
+    await sequelize.close();
+  });
+
+  describe("validate", () => {
+    let user;
+
+    beforeEach(() => {
+      user = User.build(mockUser);
+    });
+
+    test("should not allow null `firstName`", async () => {
+      user.firstName = null;
+      await expect(user.validate(["firstName"])).rejects.toThrow(
+        "nameRequired"
+      );
+    });
+
+    test("should not allow empty `firstName`", async () => {
+      user.firstName = " ";
+      await expect(user.validate(["firstName"])).rejects.toThrow("emptyName");
+    });
+
+    test("should not allow invalid `email`", async () => {
+      user.email = "siuuuuu";
+      await expect(user.validate(["email"])).rejects.toThrow("invalidEmail");
+    });
+
+    test("should not allow invalid `phoneNumber`", async () => {
+      user.phoneNumber = " ";
+      await expect(user.validate(["phoneNumber"])).rejects.toThrow(
+        "invalidPhoneNumber"
+      );
+    });
+
+    test("should not allow invalid `locale`", async () => {
+      user.locale = "1234";
+      await expect(user.validate(["phoneNumber"])).rejects.toThrow(
+        "invalidLocale"
+      );
+    });
   });
 });
