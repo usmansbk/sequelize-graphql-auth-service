01d35fc6665c23393c8c3426c241d2237f6fe587
Usman Suleiman <usmansbk@gmail.com>
2021-12-12 21:24:58 +0100

    Hook error

--- src/graphql/datasources/user.js
+++ src/graphql/datasources/user.js
@@ -1,6 +1,11 @@
 import SequelizeDataSource from "./SequelizeDataSource";
 
 export default class UserDS extends SequelizeDataSource {
+  onError(error) {
+    // format error
+    super.onError(error);
+  }
+
   async createWithEmail(fields) {
     let user = await this.create(fields);
 
--- src/graphql/resolvers/auth.js
+++ src/graphql/resolvers/auth.js
@@ -1,6 +1,22 @@
 export default {
   Mutation: {
-    async registerWithEmail(_, { input }, { dataSources }) {},
+    async registerWithEmail(_, { input }, { dataSources }) {
+      try {
+        const user = await dataSources.users.createWithEmail(input);
+        // generate token
+        return {
+          success: true,
+          message: `Welcome, ${user.firstName}!`,
+          token: "mockToken",
+        };
+      } catch ({ errors, message }) {
+        return {
+          success: false,
+          message,
+          errors,
+        };
+      }
+    },
     resendConfirmationEmail() {},
     confirmEmail() {},
     loginWithEmail() {},
