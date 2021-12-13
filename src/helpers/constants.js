4a0c4f49f9c795c9c9f3217159e5956c7424e54e
Usman Suleiman <usmansbk@gmail.com>
2021-12-13 08:35:48 +0100

    Extract constants

---
+++ src/helpers/constants.js
@@ -0,0 +1,13 @@
+export const FIRST_NAME_LEN = "firstNameLength";
+export const FIRST_NAME_REQUIRED = "firstNameRequired";
+export const FIRST_NAME_EMPTY = "firstNameEmpty";
+export const LAST_NAME_LEN = "lastNameLength";
+export const LAST_NAME_REQUIRED = "lastNameRequired";
+export const LAST_NAME_EMPTY = "lastNameEmpty";
+export const EMAIL_UNAVAILABLE = "emailUsed";
+export const INVALID_EMAIL = "invalidEmail";
+export const PHONE_NUMBER_UNAVAILABLE = "phoneNumberUnavailable";
+export const INVALID_PHONE_NUMBER = "invalidPhoneNumber";
+export const PASSWORD_LEN = "invalidPasswordLength";
+export const INVALID_PASSWORD = "invalidPassword";
+export const INVALID_LOCALE = "invalidLocale";
