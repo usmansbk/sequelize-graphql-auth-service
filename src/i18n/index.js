84c7f72b4a3e510735dae3287ffe18e1e0236f1f
Usman Suleiman <usmansbk@gmail.com>
2021-12-13 12:36:20 +0100

    Setup i18n

---
+++ src/i18n/index.js
@@ -0,0 +1,14 @@
+import i18next from "i18next";
+import en from "./locales/en";
+
+i18next.init({
+  fallbackLng: "en",
+  debug: process.env.NODE_ENV === "development",
+  resources: {
+    en: {
+      translation: en,
+    },
+  },
+});
+
+export default (lng) => i18next.getFixedT(lng);
