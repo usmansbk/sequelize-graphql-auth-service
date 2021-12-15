61637fdad4e01c64f6eafb03a09163b3f0d86d48
Usman Suleiman <usmansbk@gmail.com>
2021-12-15 19:27:48 +0100

    fix language type

--- tests/factories/user.js
+++ tests/factories/user.js
@@ -10,7 +10,7 @@ const attributes = (attr = {}) => {
     email: faker.internet.email(),
     password: faker.internet.password(6),
     phoneNumber: faker.phone.phoneNumber(),
-    locale: "en",
+    language: "en",
   };
 
   return Object.assign({}, defaults, attr);
