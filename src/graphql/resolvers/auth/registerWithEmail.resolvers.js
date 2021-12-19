14179cd663683b8ccad0a1bd624d923ffd1ffebe
Usman Suleiman <usmansbk@gmail.com>
2021-12-19 18:06:10 +0100

    Delete unverified email

--- src/graphql/resolvers/auth/registerWithEmail.resolvers.js
+++ src/graphql/resolvers/auth/registerWithEmail.resolvers.js
@@ -1,38 +1,24 @@
 import MutationError from "~utils/errors/MutationError";
 import { WELCOME_NEW_USER } from "~helpers/constants";
-import { hostURL } from "~helpers/url";
-import sendMail from "~services/mailer";
 
 export default {
   Mutation: {
-    async registerWithEmail(
-      _,
-      { input },
-      { dataSources, jwt, t, locale, redis }
-    ) {
+    async registerWithEmail(_, { input }, { dataSources, jwt, t, redis }) {
       try {
-        const { firstName, language, email } =
+        const { id, firstName, language } =
           await dataSources.users.createWithEmail(input);
 
-        const { token, ex } = jwt.getToken(2);
-
-        await redis.setex(email, ex, token);
-
-        sendMail({
-          template: "verify_email",
-          message: {
-            to: email,
-          },
-          locals: {
-            locale: language || locale,
-            name: firstName,
-            link: hostURL(`/verify_email?token=${token}`),
-          },
+        const { accessToken, refreshToken, tokenId, ex } = jwt.getAuthTokens({
+          id,
+          language,
         });
+        await redis.setex(tokenId, ex, refreshToken); // refresh token rotation
 
         return {
           success: true,
           message: t(WELCOME_NEW_USER, { firstName }),
+          accessToken,
+          refreshToken,
         };
       } catch (e) {
         if (e instanceof MutationError) {
