309d6d4f8d50d4ea72d137d1eb0495bc1e0271db
Usman Suleiman Babakolo <usmansbk@gmail.com>
2022-01-12 18:03:32 +0100

    Add comment

--- src/utils/errors/QueryError.js
+++ src/utils/errors/QueryError.js
@@ -1,5 +1,8 @@
 import AbstractError from "./AbstractError";
 
+/**
+ * This class represents both "mutation" and "query" errors.
+ */
 export default class QueryError extends AbstractError {
   constructor(message, cause) {
     super(message);
