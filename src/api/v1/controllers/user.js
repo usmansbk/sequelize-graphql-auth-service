5acbc8becb4dc00ddf8737846555bc789f2fd564
Usman Suleiman Babakolo <usmansbk@gmail.com>
2022-01-14 19:37:22 +0100

    upload locally

--- src/api/v1/controllers/user.js
+++ src/api/v1/controllers/user.js
@@ -1,14 +1,13 @@
 const picture = (req, res) => {
-  console.log(req.body);
   const { filename, size, mimetype, path } = req.file;
 
   res.send({
     success: true,
     file: {
-      fileName: filename,
       size,
-      mimeType: mimetype,
       path,
+      fileName: filename,
+      mimeType: mimetype,
     },
   });
 };
