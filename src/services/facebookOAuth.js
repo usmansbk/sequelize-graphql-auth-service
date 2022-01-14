de4aaac30c43f6f78c783d6719903b338b9476be
Usman Suleiman Babakolo <usmansbk@gmail.com>
2022-01-14 13:14:56 +0100

    verify facebook token

--- src/services/facebookOAuth.js
+++ src/services/facebookOAuth.js
@@ -1,3 +1,38 @@
-const verifyFacebookToken = async () => {};
+import fetch from "node-fetch";
+import { TOKEN_INVALID_ERROR } from "~helpers/constants/i18n";
+import TokenError from "~utils/errors/TokenError";
+
+const HOST_URL = "graph.facebook.com";
+const fields = [
+  "id",
+  "first_name",
+  "last_name",
+  "short_name",
+  "email",
+  "picture{url}",
+];
+
+const verifyFacebookToken = async (accessToken) => {
+  const req = `https://${HOST_URL}/me?fields=${fields.join(
+    ","
+  )}&access_token=${accessToken}`;
+  const res = await fetch(req);
+  const body = await res.json();
+  if (body?.error) {
+    console.log(body.error);
+    throw new TokenError(TOKEN_INVALID_ERROR);
+  }
+
+  console.log(body);
+
+  return {
+    firstName: body.first_name,
+    lastName: body.last_name,
+    nickName: body.short_name,
+    email: body.email,
+    socialAvatarURL: body.picture?.data?.url,
+    emailVerified: true,
+  };
+};
 
 export default verifyFacebookToken;
