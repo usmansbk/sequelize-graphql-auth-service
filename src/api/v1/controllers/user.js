81afd026156a182f448ecfd54977537b17940270
Usman Suleiman Babakolo <usmansbk@gmail.com>
2022-01-14 19:31:37 +0100

    add local upload

--- src/api/v1/controllers/user.js
+++ src/api/v1/controllers/user.js
@@ -1,4 +1,17 @@
-const picture = () => {};
+const picture = (req, res) => {
+  console.log(req.body);
+  const { filename, size, mimetype, path } = req.file;
+
+  res.send({
+    success: true,
+    file: {
+      fileName: filename,
+      size,
+      mimeType: mimetype,
+      path,
+    },
+  });
+};
 
 export default {
   picture,
--- src/api/v1/routes/user.js
+++ src/api/v1/routes/user.js
@@ -1,8 +1,9 @@
 import express from "express";
+import upload from "~middlewares/upload";
 import UserController from "../controllers/user";
 
 const router = express.Router();
 
-router.post("/picture", UserController.picture);
+router.post("/picture", upload.single("picture"), UserController.picture);
 
 export default router;
