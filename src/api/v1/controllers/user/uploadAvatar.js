fdf7b4603ce2046863138b364453a58b71906689
Usman Suleiman Babakolo <usmansbk@gmail.com>
2022-01-15 10:36:20 +0100

    change field name

--- src/api/v1/controllers/user/uploadAvatar.js
+++ src/api/v1/controllers/user/uploadAvatar.js
@@ -5,7 +5,7 @@ import { IMAGE_TOO_LARGE } from "~helpers/constants/i18n";
 import { PROFILE_PICTURE_MAX_FILE_SIZE } from "~helpers/constants/upload";
 import { BYTES } from "~helpers/constants/numeral";
 
-const upload = uploadProfilePicture.single("picture");
+const upload = uploadProfilePicture.single("avatar");
 // import db from "~db/models";
 
 // const { User } = db;
