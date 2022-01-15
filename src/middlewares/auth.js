6225bec80652ae52c92790cf2276819460069109
Usman Suleiman Babakolo <usmansbk@gmail.com>
2022-01-15 02:17:23 +0100

    Fix middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,20 +1,13 @@
 import * as jwt from "~utils/jwt";
-import { UNAUTHORIZED } from "~helpers/constants/i18n";
 
-const auth = (req, res, next) => {
-  const token = req.headers.authorization;
-
-  if (!token) {
-    res.status(401).json({
-      succes: false,
-      message: req.t(UNAUTHORIZED),
-    });
-    return;
+const auth = (req, _res, next) => {
+  try {
+    const token = req.headers.authorization;
+    const userInfo = jwt.verify(token);
+    req.userInfo = userInfo;
+  } catch (e) {
+    next(e);
   }
-
-  const userInfo = jwt.verify(token);
-  req.userInfo = userInfo;
-  next();
 };
 
 export default auth;
