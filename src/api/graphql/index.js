51c78c8544682d0d46ad7f4450743fb5f116c493
Usman Suleiman Babakolo <usmansbk@gmail.com>
2022-01-15 23:23:20 +0100

    chore

--- src/api/graphql/index.js
+++ src/api/graphql/index.js
@@ -1,5 +1,6 @@
 import { ApolloServer } from "apollo-server-express";
 import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
+import { makeExecutableSchema } from "@graphql-tools/schema";
 import http from "http";
 import db from "~db/models";
 import log from "~utils/logger";
@@ -11,11 +12,19 @@ import typeDefs from "./typeDefs";
 import resolvers from "./resolvers";
 import { UserDS, FileDS } from "./datasources";
 
+const createSchema = () => {
+  const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers,
+  });
+
+  return schema;
+};
+
 export const createApolloServer = (app) => {
   const httpServer = http.createServer(app);
   const server = new ApolloServer({
-    typeDefs,
-    resolvers,
+    schema: createSchema(),
     plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
     logger: log,
     dataSources: () => ({
@@ -30,6 +39,7 @@ export const createApolloServer = (app) => {
         try {
           tokenInfo = jwt.verify(accessToken);
           if (tokenInfo?.lng) {
+            // check if logged in user has a preferred language and use it
             await req.i18n.changeLanguage(tokenInfo.lng);
           }
         } catch (e) {
@@ -42,7 +52,7 @@ export const createApolloServer = (app) => {
         store,
         tokenInfo,
         t: req.t,
-        locale: req.locale,
+        locale: req.locale, // we still need default locale for when user is not logged in, E.g reset password email
         clientId: req.headers.client_id,
         accessToken,
         otp,
