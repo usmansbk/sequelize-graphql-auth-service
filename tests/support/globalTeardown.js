6e4a81a9740656418dbd2de12b92a681f2fd1ed3
Usman Suleiman Babakolo <usmansbk@gmail.com>
2022-01-15 23:38:20 +0100

    Fix missing async

--- tests/support/globalTeardown.js
+++ tests/support/globalTeardown.js
@@ -2,7 +2,7 @@ import db from "~db/models";
 
 const { sequelize } = db;
 
-const globalTeardown = () => {
+const globalTeardown = async () => {
   await sequelize.drop();
   await sequelize.close();
 };
