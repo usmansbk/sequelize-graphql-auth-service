b29fe3be54450a9f0e113fa2de6b2811ceceb6ff
Usman Suleiman Babakolo <usmansbk@gmail.com>
2022-01-16 10:25:36 +0100

    Set timeout

--- src/api/v1/routes/auth.js
+++ src/api/v1/routes/auth.js
@@ -1,8 +1,9 @@
 import express from "express";
+import timeout from "connect-timeout";
 import AuthController from "../controllers/auth";
 
 const router = express.Router();
 
-router.post("/refresh_token", AuthController.refreshToken);
+router.post("/refresh_token", timeout("15s"), AuthController.refreshToken);
 
 export default router;
