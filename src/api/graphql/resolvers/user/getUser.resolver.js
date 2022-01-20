ca0b896226e8ddbc49d81f8eadc98f4e39a27c5b
Usman Suleiman Babakolo <usmansbk@gmail.com>
2022-01-20 13:59:26 +0100

    create roles table

--- src/api/graphql/resolvers/user/getUser.resolver.js
+++ src/api/graphql/resolvers/user/getUser.resolver.js
@@ -1,11 +1,16 @@
 import { Fail, Success } from "~helpers/response";
 import QueryError from "~utils/errors/QueryError";
+import { USER_DOES_NOT_EXIST } from "~helpers/constants/i18n";
 
 export default {
   Query: {
     async getUser(_parent, { id }, { dataSources, t }) {
       try {
         const user = await dataSources.users.findByPk(id);
+
+        if (!user) {
+          throw new QueryError(USER_DOES_NOT_EXIST);
+        }
 
         return Success({ user });
       } catch (e) {
