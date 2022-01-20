a0d8028e912e1718d008883eb1f2dac334e70615
Usman Suleiman Babakolo <usmansbk@gmail.com>
2022-01-20 09:36:34 +0100

    create failed login email template

--- src/helpers/emailTemplates.js
+++ src/helpers/emailTemplates.js
@@ -3,6 +3,7 @@ const emailTemplates = {
   RESET_PASSWORD: "reset_password",
   CONFIRM_DELETE_ACCOUNT: "confirm_delete_account",
   OTP: "otp",
+  FAILED_LOGIN: "failed_login",
 };
 
 export default emailTemplates;
