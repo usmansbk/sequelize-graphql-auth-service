24314f2d1bf213dc686e93f732c945ed66cca8f6
Usman Suleiman Babakolo <usmansbk@gmail.com>
2022-01-23 11:14:34 +0100

    refactor

--- tests/integration/apolloServer.js
+++ tests/integration/apolloServer.js
@@ -2,12 +2,10 @@ import { ApolloServer } from "apollo-server-express";
 import { createSchema } from "~api/graphql";
 import dataSources from "~api/graphql/datasources";
 import otp from "~utils/otp";
-import * as jwt from "~utils/jwt";
+import jwt from "~utils/jwt";
 import mailer from "~utils/mailer";
 import store from "~utils/store";
 import fileStorage from "~utils/fileStorage";
-
-jest.mock("~utils/mailer");
 
 const createApolloTestServer = () => {
   const schema = createSchema();
