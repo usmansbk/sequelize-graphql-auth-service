2db270a3588e58c93d457ffc2dde304fe7ab37af
Usman Suleiman Babakolo <usmansbk@gmail.com>
2022-01-29 00:07:47 +0100

    chore

--- src/api/graphql/resolvers/auth/logout.resolver.js
+++ src/api/graphql/resolvers/auth/logout.resolver.js
@@ -1,15 +1,22 @@
 import { Success } from "~helpers/response";
 import { LOGGED_OUT } from "~helpers/constants/i18n";
+import { allowedClients } from "~helpers/constants/auth";
 
 export default {
   Mutation: {
     // Log out is idempotent
-    async logout(_parent, _args, { store, t, accessToken, jwt, clientId }) {
+    async logout(_parent, { all }, { store, t, accessToken, jwt, clientId }) {
       if (accessToken) {
-        const tokenInfo = jwt.decode(accessToken);
+        const { sub } = jwt.decode(accessToken);
 
-        // No refresh token means the user never logged in
-        await store.remove(`${clientId}:${tokenInfo.sub}`);
+        // delete session
+        if (all) {
+          allowedClients.forEach((aud) => {
+            store.remove(`${aud}:${sub}`);
+          });
+        } else {
+          store.remove(`${clientId}:${sub}`);
+        }
       }
 
       return Success({
