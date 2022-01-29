f2ae81f28be7b3e35d0a88fb3baed49e1c3f938c
Usman Suleiman Babakolo <usmansbk@gmail.com>
2022-01-29 18:20:35 +0100

    test logout

--- tests/integration/resolvers/auth/logout.spec.js
+++ tests/integration/resolvers/auth/logout.spec.js
@@ -1,6 +1,9 @@
 import { gql } from "apollo-server-express";
 import db from "~db/models";
 import createApolloTestServer from "tests/integration/apolloServer";
+import attributes from "tests/attributes";
+import jwt from "utils/jwt";
+import store from "utils/store";
 
 const query = gql`
   mutation Logout($all: Boolean) {
@@ -23,26 +26,53 @@ describe("Mutation.logout", () => {
     await db.sequelize.close();
   });
 
-  test("should logout user from current client", async () => {
-    const res = await server.executeOperation({ query });
-    expect(res.data.logout).toEqual({
-      code: "LoggedOut",
-      success: true,
-      message: "LoggedOut",
+  let user;
+  let accessToken;
+  const clientId = "test";
+  beforeEach(async () => {
+    user = await db.User.create(attributes.user());
+    const tokens = jwt.generateAuthTokens({
+      aud: clientId,
+      sub: user.id,
     });
+    accessToken = tokens.accessToken;
+    await store.set({ key: `${clientId}:${user.id}`, value: tokens.sid });
   });
 
-  test("should logout user from all clients", async () => {
-    const res = await server.executeOperation({
-      query,
-      variables: {
-        all: true,
-      },
-    });
+  test("should clear current user session", async () => {
+    const res = await server.executeOperation(
+      { query },
+      { accessToken, clientId }
+    );
+    const sessionId = await store.get(`${clientId}:${user.id}`);
     expect(res.data.logout).toEqual({
       code: "LoggedOut",
       success: true,
       message: "LoggedOut",
     });
+    expect(sessionId).toBe(null);
+  });
+
+  test("should clear all current user sessions", async () => {
+    const res = await server.executeOperation(
+      {
+        query,
+        variables: {
+          all: true,
+        },
+      },
+      {
+        accessToken,
+        clientId,
+      }
+    );
+    const sessionId = await store.get(`${clientId}:${user.id}`);
+
+    expect(res.data.logout).toEqual({
+      code: "LoggedOut",
+      success: true,
+      message: "LoggedOut",
+    });
+    expect(sessionId).toBe(null);
   });
 });
