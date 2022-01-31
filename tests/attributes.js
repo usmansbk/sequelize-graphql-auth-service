cd0a7579b59d5ddf1770469420bd8d02b31ff127
Usman Suleiman Babakolo <usmansbk@gmail.com>
2022-01-31 22:45:21 +0100

    unique emails

--- tests/attributes.js
+++ tests/attributes.js
@@ -3,7 +3,7 @@ import faker from "@faker-js/faker";
 const user = (fields = {}) => ({
   firstName: faker.name.firstName(),
   lastName: faker.name.lastName(),
-  email: faker.internet.email(),
+  email: faker.unique(faker.internet.email),
   password: faker.internet.password(6),
   ...fields,
 });
