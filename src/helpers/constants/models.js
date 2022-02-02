2a996b7550ab1040060099629397a4dec5893415
Usman Suleiman Babakolo <usmansbk@gmail.com>
2022-02-02 09:32:55 +0100

    chore

--- src/helpers/constants/models.js
+++ src/helpers/constants/models.js
@@ -1,9 +1,7 @@
 // aliases
-export const AVATAR_ALIAS = "avatar";
 export const ROLES_ALIAS = "roles";
 
 // foreign keys
-export const USER_AVATAR_FOREIGN_KEY = "avatarId";
 export const USER_ROLE_FOREIGN_KEY = "roleId";
 
 // join tables
