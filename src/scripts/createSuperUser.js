d9f48f7407601bbbc8c5f4baa83a36f653dec967
Usman Suleiman Babakolo <usmansbk@gmail.com>
2022-02-05 12:14:33 +0100

    add create super user script

---
+++ src/scripts/createSuperUser.js
@@ -0,0 +1,33 @@
+import db from "~db/models";
+import log from "~utils/logger";
+
+const { sequelize, User, Role } = db;
+const createSuperUser = async () => {
+  try {
+    await sequelize.authenticate();
+    const [role] = await Role.findOrCreate({
+      where: { name: "ADMIN" },
+      defaults: {
+        description: "Admin group",
+      },
+    });
+    const [user] = await User.findOrCreate({
+      where: {
+        username: "admin",
+      },
+      defaults: {
+        email: "usmansbk.dev@gmail.com",
+        firstName: "Usman",
+        lastName: "Suleiman",
+        password: "admin123",
+      },
+    });
+    await user.addRole(role);
+  } catch (e) {
+    log.error(e);
+  }
+};
+
+createSuperUser();
+
+export default createSuperUser;
