bf7a673ee3e1ff283d2bbaff802cc4e1a58f19c1
Usman Suleiman Babakolo <usmansbk@gmail.com>
2022-03-17 21:28:24 +0100

    bump

--- src/scripts/createSuperUser.js
+++ src/scripts/createSuperUser.js
@@ -42,7 +42,7 @@ const createSuperUser = async () => {
       email: "usmansbk.dev@gmail.com",
       firstName: "Usman",
       lastName: "Suleiman",
-      username: "usman",
+      username: "admin",
       password: "qwertyadmin48",
     });
     await admin.addRole(role);
