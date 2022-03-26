b56341ef700e9a08f4e37489d33023967c4dd42b
Usman Suleiman Babakolo <usmansbk@gmail.com>
2022-03-26 22:24:56 +0100

    nit

--- src/api/graphql/resolvers/user/user.resolver.js
+++ src/api/graphql/resolvers/user/user.resolver.js
@@ -1,3 +1,6 @@
+import { Fail, Success } from "~helpers/response";
+import QueryError from "~utils/errors/QueryError";
+
 export default {
   User: {
     picture(parent) {
@@ -5,6 +8,30 @@ export default {
     },
     isOwner(parent, _args, { tokenInfo }) {
       return parent.id === tokenInfo?.sub;
+    },
+  },
+  Query: {
+    async me(_parent, _args, { t, currentUser }) {
+      try {
+        return Success({ user: currentUser });
+      } catch (e) {
+        if (e instanceof QueryError) {
+          return Fail({
+            message: t(e.message),
+            code: e.code,
+          });
+        }
+        throw e;
+      }
+    },
+    async users(_parent, _args, { dataSources }) {
+      const items = await dataSources.users.findAll();
+      const totalCount = items.length;
+
+      return {
+        items,
+        totalCount,
+      };
     },
   },
 };
