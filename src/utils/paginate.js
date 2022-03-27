92bd43a13856185ffafadf541bc1ca71da428aa9
Usman Suleiman Babakolo <usmansbk@gmail.com>
2022-03-27 23:01:59 +0100

    add docs

--- src/utils/paginate.js
+++ src/utils/paginate.js
@@ -5,4 +5,8 @@ export const getNextCursor = (data = {}) => btoa(JSON.stringify(data));
 
 export const parseCursor = (cursor) => JSON.parse(atob(cursor));
 
+/**
+ * Based on
+ * [MySQL cursor based pagination with multiple columns](https://stackoverflow.com/questions/38017054/mysql-cursor-based-pagination-with-multiple-columns/38017813)
+ */
 export const paginate = () => {};
