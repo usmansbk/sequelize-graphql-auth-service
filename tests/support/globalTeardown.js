087e97155726b232861cf0d34f9e84bdb7012bc1
Usman Suleiman Babakolo <usmansbk@gmail.com>
2022-03-28 15:20:09 +0100

    nit

--- tests/support/globalTeardown.js
+++ tests/support/globalTeardown.js
@@ -1,9 +1,12 @@
 import db from "~db/models";
+import store from "~utils/store";
 
 const { sequelize } = db;
 
 const globalTeardown = async () => {
   await sequelize.close();
+  await store.clearAll();
+  store.close();
 };
 
 export default globalTeardown;
