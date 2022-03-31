d1d318911039ec5ca26d54edcf6a86c843241166
Usman Suleiman Babakolo <usmansbk@gmail.com>
2022-03-31 20:26:02 +0100

    nit

--- src/api/graphql/resolvers/role/role.resolver.js
+++ src/api/graphql/resolvers/role/role.resolver.js
@@ -1,10 +1,10 @@
 export default {
   Query: {
-    roles(_parent, { page }, { dataSources }) {
-      return dataSources.roles.paginate({ page });
-    },
+    roles: (_parent, { page }, { dataSources }) =>
+      dataSources.roles.paginate({ page }),
   },
   Mutation: {
-    deleteRoles: (_, { ids }, { dataSources }) => dataSources.roles.destroyMany(ids),
+    deleteRoles: (_, { ids }, { dataSources }) =>
+      dataSources.roles.destroyMany(ids),
   },
 };
