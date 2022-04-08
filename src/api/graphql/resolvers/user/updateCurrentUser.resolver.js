5aa6bdca232a9bfeb7e1c512c823d21ee399c499
usmansbk <usmansbk@gmail.com>
2022-04-08 23:04:37 +0100

    update schema

--- src/api/graphql/resolvers/user/updateCurrentUser.resolver.js
+++ src/api/graphql/resolvers/user/updateCurrentUser.resolver.js
@@ -64,7 +64,7 @@ export default {
         throw e;
       }
     },
-    async removeCurrentUserProfilePicture(
+    async removeCurrentUserAvatar(
       _parent,
       _args,
       { currentUser, t, dataSources }
