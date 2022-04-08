98fe751b3dd96eec2eea853327603e492b2b6afb
usmansbk <usmansbk@gmail.com>
2022-04-08 22:53:11 +0100

    add resolvers

--- src/api/graphql/resolvers/user/admin.resolver.js
+++ src/api/graphql/resolvers/user/admin.resolver.js
@@ -8,43 +8,9 @@ export default {
     },
   },
   Mutation: {
-    async createUserAccounts(
-      _parent,
-      { input: { profiles, roleIds } },
-      { dataSources, db, t }
-    ) {
+    async createUser(_parent, { input }, { dataSources, t }) {
       try {
-        const users = await db.sequelize.transaction(async (transaction) => {
-          const newUsers = await dataSources.users.createMany(profiles, {
-            transaction,
-          });
-          if (roleIds?.length) {
-            await Promise.all(
-              newUsers.map((user) => user.addRoles(roleIds, { transaction }))
-            );
-          }
-          return newUsers;
-        });
-        return Success({ users });
-      } catch (e) {
-        if (e instanceof QueryError) {
-          return Fail({
-            message: t(e.message),
-            errors: e.errors,
-            code: e.code,
-          });
-        }
-
-        throw e;
-      }
-    },
-    async updateUserProfile(
-      _parent,
-      { input: { id, ...values } },
-      { dataSources, t }
-    ) {
-      try {
-        const user = await dataSources.users.update(id, values);
+        const user = await dataSources.users.create(input);
         return Success({ user });
       } catch (e) {
         if (e instanceof QueryError) {
@@ -54,11 +20,127 @@ export default {
             code: e.code,
           });
         }
-
         throw e;
       }
     },
-    async changeAccountStatus(
+    async changeUserFullname(
+      _parent,
+      { input: { id, firstName, lastName } },
+      { dataSources, t }
+    ) {
+      try {
+        const user = await dataSources.users.update(id, {
+          firstName,
+          lastName,
+        });
+        return Success({ user });
+      } catch (e) {
+        if (e instanceof QueryError) {
+          return Fail({
+            message: t(e.message),
+            errors: e.errors,
+            code: e.code,
+          });
+        }
+        throw e;
+      }
+    },
+    async changeUserUsername(
+      _parent,
+      { input: { id, username } },
+      { dataSources, t }
+    ) {
+      try {
+        const user = await dataSources.users.update(id, { username });
+        return Success({ user });
+      } catch (e) {
+        if (e instanceof QueryError) {
+          return Fail({
+            message: t(e.message),
+            errors: e.errors,
+            code: e.code,
+          });
+        }
+        throw e;
+      }
+    },
+    async changeUserLocale(
+      _parent,
+      { input: { id, locale } },
+      { dataSources, t }
+    ) {
+      try {
+        const user = await dataSources.users.update(id, { locale });
+        return Success({ user });
+      } catch (e) {
+        if (e instanceof QueryError) {
+          return Fail({
+            message: t(e.message),
+            errors: e.errors,
+            code: e.code,
+          });
+        }
+        throw e;
+      }
+    },
+    async changeUserEmail(
+      _parent,
+      { input: { id, email } },
+      { dataSources, t }
+    ) {
+      try {
+        const user = await dataSources.users.update(id, { email });
+        return Success({ user });
+      } catch (e) {
+        if (e instanceof QueryError) {
+          return Fail({
+            message: t(e.message),
+            errors: e.errors,
+            code: e.code,
+          });
+        }
+        throw e;
+      }
+    },
+    async changeUserPhoneNumber(
+      _parent,
+      { input: { id, phoneNumber } },
+      { dataSources, t }
+    ) {
+      try {
+        const user = await dataSources.users.update(id, { phoneNumber });
+        return Success({ user });
+      } catch (e) {
+        if (e instanceof QueryError) {
+          return Fail({
+            message: t(e.message),
+            errors: e.errors,
+            code: e.code,
+          });
+        }
+        throw e;
+      }
+    },
+    async changeUserPassword(
+      _parent,
+      { input: { id, password } },
+      { dataSources, t }
+    ) {
+      try {
+        const user = await dataSources.users.update(id, { password });
+        return Success({ user });
+      } catch (e) {
+        if (e instanceof QueryError) {
+          return Fail({
+            message: t(e.message),
+            errors: e.errors,
+            code: e.code,
+          });
+        }
+        throw e;
+      }
+    },
+    async changeUserStatus(
       _parent,
       { input: { id, status } },
       { dataSources, t }
@@ -74,14 +156,32 @@ export default {
             code: e.code,
           });
         }
-
         throw e;
       }
     },
-    async deleteUserAccounts(_parent, { ids }, { dataSources, t }) {
+    async removeUserAvatar(_parent, { id }, { t, dataSources }) {
       try {
-        await dataSources.users.destroyMany(ids);
-        return Success({ ids });
+        const user = await dataSources.users.findByPk(id);
+        const avatar = await user.getAvatar();
+        if (avatar) {
+          await dataSources.files.destroy(avatar.id);
+        }
+
+        return Success({ user });
+      } catch (e) {
+        if (e instanceof QueryError) {
+          return Fail({
+            message: t(e.message),
+            code: e.code,
+          });
+        }
+        throw e;
+      }
+    },
+    async deleteUser(_parent, { id }, { dataSources, t }) {
+      try {
+        await dataSources.users.destroy(id);
+        return Success({ id });
       } catch (e) {
         if (e instanceof QueryError) {
           return Fail({
@@ -90,7 +190,6 @@ export default {
             code: e.code,
           });
         }
-
         throw e;
       }
     },
@@ -174,7 +273,6 @@ export default {
             code: e.code,
           });
         }
-
         throw e;
       }
     },
