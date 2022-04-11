074308d06fec7eb9faf5edaae6a4fc23bc9d5947
usmansbk <usmansbk@gmail.com>
2022-04-11 18:42:18 +0100

    add graphql sentry error plugin

--- src/api/graphql/plugins/ErrorPlugin.js
+++ src/api/graphql/plugins/ErrorPlugin.js
@@ -2,6 +2,7 @@ import analytics from "~services/analytics";
 import Sentry from "~services/sentry";
 
 /**
+ * https://blog.sentry.io/2020/07/22/handling-graphql-errors-using-sentry
  * This plugin translates the errors to the client request language
  * and logs errors to analytics
  */
@@ -9,10 +10,29 @@ const errorPlugin = {
   async requestDidStart() {
     return {
       async didEncounterErrors(requestContext) {
-        const { errors, context } = requestContext;
+        const { errors, context, operation, request } = requestContext;
         analytics.flush();
         errors.forEach((e) => {
-          Sentry.captureException(e);
+          Sentry.withScope((scope) => {
+            // Annotate whether failing operation was query/mutation/subscription
+            scope.setTag("kind", operation.operation);
+            // Log query and variables as extras
+            // (make sure to strip out sensitive data!)
+            scope.setExtra("query", request.query);
+            scope.setExtra("variables", request.variables);
+            if (e.path) {
+              scope.addBreadcrumb({
+                category: "query-path",
+                message: e.path.join(" > "),
+                level: Sentry.Severity.Debug,
+              });
+            }
+            const transactionId = request.http.headers.get("x-transaction-id");
+            if (transactionId) {
+              scope.setTransactionName(transactionId);
+            }
+            Sentry.captureException(e);
+          });
           e.message = context.t(e.message);
         });
       },
