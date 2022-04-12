5453f0bc8ea25e9e06bb7438f726e6b6cd51eb09
usmansbk <usmansbk@gmail.com>
2022-04-12 10:15:29 +0100

    nit

--- src/helpers/getUser.js
+++ src/helpers/getUser.js
@@ -7,16 +7,20 @@ const getUser = async (id) => {
   const key = `auth:${id}`;
   const cached = await cache.get(key);
 
+  const loadFromDb = async () => {
+    const user = await db.User.scope("roles").findByPk(id);
+    await cache.set({
+      key,
+      value: JSON.stringify(user.roles),
+      expiresIn: 5 * 60, // 5 minutes
+    });
+    return user.roles;
+  };
+
   if (cached) {
     userRoles = JSON.parse(cached);
   } else {
-    const user = await db.User.scope("roles").findByPk(id);
-    userRoles = user.roles;
-    await cache.set({
-      key,
-      value: JSON.stringify(userRoles),
-      expiresIn: 5 * 60, // 5 minutes
-    });
+    userRoles = (await loadFromDb()).roles;
   }
 
   return {
