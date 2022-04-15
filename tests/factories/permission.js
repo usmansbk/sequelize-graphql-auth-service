ff0b8934c07d0b5e3d6cb73b338ea1707056bd99
usmansbk <usmansbk@gmail.com>
2022-04-15 23:53:15 +0100

    test

--- tests/factories/permission.js
+++ tests/factories/permission.js
@@ -4,6 +4,7 @@ const define = {
   modelName: "Permission",
   attributes: () => ({
     scope: faker.unique(faker.random.word),
+    description: "A short description",
   }),
   associations: {
     roles: "role",
