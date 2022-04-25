1193d49b470eeac3d72b400b91420b7e84ced468
usmansbk <usmansbk@gmail.com>
2022-04-25 07:51:39 +0100

    nit

--- src/constants/countries.js
+++ src/constants/countries.js
@@ -2,9 +2,9 @@ const countries = [
   {
     name: "Nigeria",
     code: "NG",
-    dialCode: "234",
-    phoneNumberPattern: "(+234)### ### ####",
-    phoneNumberPlaceholder: "(234)806 314 8541",
+    dialCode: "+234",
+    phoneNumberPattern: "+234 (###) ### ####",
+    phoneNumberPlaceholder: "806 314 8541",
   },
 ];
 
