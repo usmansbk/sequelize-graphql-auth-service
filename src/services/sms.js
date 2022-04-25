3a08de21590e02dae69fb457680fcdf310c56ce2
usmansbk <usmansbk@gmail.com>
2022-04-25 19:39:02 +0100

    nit

--- src/services/sms.js
+++ src/services/sms.js
@@ -9,11 +9,7 @@ const sendSMS = async (message, to) => {
     return;
   }
 
-  if (process.env.NODE_ENV === "development") {
-    log.info(to, message);
-    return;
-  }
-
+  log.info(to, message);
   const client = twilio(accountSid, authToken);
   const response = await client.messages.create({
     body: message,
