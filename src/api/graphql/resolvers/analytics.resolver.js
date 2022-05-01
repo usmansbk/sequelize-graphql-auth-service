91d07d0997035edc0f2d21bcc574b8e65afe5d63
usmansbk <usmansbk@gmail.com>
2022-05-01 06:18:00 +0100

    nit

--- src/api/graphql/resolvers/analytics.resolver.js
+++ src/api/graphql/resolvers/analytics.resolver.js
@@ -3,7 +3,8 @@ export default {
     totalUserCount(_parent, _arg, { dataSources }) {
       return dataSources.users.count();
     },
-    newUserCount(_parent, { since }, { dataSources }) {
+    newUserCount(_parent, arg, { dataSources }) {
+      const { since } = arg || {};
       return dataSources.users.count({
         where: {
           createdAt: {
