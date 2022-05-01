9c76da593a0f2a318eb074d0eaa108db8c14556e
usmansbk <usmansbk@gmail.com>
2022-05-01 10:18:31 +0100

    query analytics data

--- src/api/graphql/resolvers/analytics.resolver.js
+++ src/api/graphql/resolvers/analytics.resolver.js
@@ -1,14 +1,17 @@
+import { Op } from "sequelize";
+import dayjs from "~utils/dayjs";
+
 export default {
   Query: {
     totalUserCount(_parent, _arg, { dataSources }) {
       return dataSources.users.count();
     },
     newUserCount(_parent, arg, { dataSources }) {
-      const { since } = arg || {};
+      const { since = dayjs.utc().startOf("day") } = arg || {};
       return dataSources.users.count({
         where: {
           createdAt: {
-            gt: since,
+            [Op.gte]: since,
           },
         },
       });
