7d43edca6211790b5ef1fea6e5fa2fb3c6edb0cd
usmansbk <usmansbk@gmail.com>
2022-05-03 08:24:52 +0100

    nit

--- src/utils/formatErrors.js
+++ src/utils/formatErrors.js
@@ -1,8 +1,11 @@
 const formatErrors = (errors, t) => {
-  const formattedErrors = errors.map(({ path, message }) => ({
-    field: path,
-    message: t(message),
-  }));
+  errors.forEach((e) => console.log(e));
+  const formattedErrors = errors.map(
+    ({ path, message, validatorArgs, validatorName }) => ({
+      field: path,
+      message: t(message, { validatorArgs, validatorName }),
+    })
+  );
 
   return formattedErrors;
 };
