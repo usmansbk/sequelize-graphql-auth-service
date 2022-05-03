56207ba7110681722cbab606cb7968345bbbe94c
usmansbk <usmansbk@gmail.com>
2022-05-03 21:10:03 +0100

    fix port

--- src/api/graphql/index.js
+++ src/api/graphql/index.js
@@ -47,7 +47,7 @@ const startApolloServer = async (app) => {
   await server.start();
   server.applyMiddleware({ app });
   await new Promise((resolve) => {
-    httpServer.listen({ port: 4000 }, resolve);
+    httpServer.listen({ port: process.env.PORT }, resolve);
   });
   return server;
 };
--- src/index.js
+++ src/index.js
@@ -34,7 +34,9 @@ const main = async () => {
     await db.sequelize.sync({ force: false });
     log.info("Database connection has been established successfully.");
     const server = await startApolloServer(app);
-    log.info(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+    log.info(
+      `🚀 Server ready at http://localhost:${process.env.PORT}${server.graphqlPath}`
+    );
   } catch (error) {
     log.error(error);
   }
