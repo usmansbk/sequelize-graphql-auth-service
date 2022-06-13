03707db7adf296eaf52799499e2c092b9148a5a8
usmansbk <usmansbk@gmail.com>
2022-06-13 16:40:07 +0100

    update deps

--- src/services/aws.js
+++ src/services/aws.js
@@ -1,14 +1,16 @@
-import aws from "aws-sdk";
+import { SESClient } from "@aws-sdk/client-ses";
+import { S3Client } from "@aws-sdk/client-s3";
 import { defaultProvider } from "@aws-sdk/credential-provider-node";
 
 const { AWS_REGION } = process.env;
 
-export const s3 = new aws.S3();
-
-export const ses = new aws.SES({
-  apiVersion: "2010-12-01",
+export const s3 = new S3Client({
   region: AWS_REGION,
-  defaultProvider,
 });
 
-export default aws;
+export const ses = new SESClient({
+  region: AWS_REGION,
+  credentialDefaultProvider: defaultProvider,
+});
+
+export default { s3, ses };
