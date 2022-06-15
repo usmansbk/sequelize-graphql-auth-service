89bede759ffb473b9155a63ef6c41a16fd75aadd
usmansbk <usmansbk@gmail.com>
2022-06-15 18:06:40 +0100

    list app script

---
+++ src/scripts/listApplications.js
@@ -0,0 +1,20 @@
+import db from "~db/models";
+import log from "~utils/logger";
+
+const { sequelize, Application } = db;
+
+const listApplications = async () => {
+  try {
+    await sequelize.sync();
+    const apps = await Application.findAll();
+
+    console.log("*********************************************");
+    apps.forEach((app) => console.log(app.name, ":", app.clientID));
+
+    await sequelize.close();
+  } catch (e) {
+    log.error(e);
+  }
+};
+
+listApplications();
