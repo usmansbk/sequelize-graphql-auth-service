443d5655c5b5dfe627d86a224fc7c00c3cbfed73
usmansbk <usmansbk@gmail.com>
2022-06-15 17:37:23 +0100

    nit

--- src/api/graphql/resolvers/application/query.resolver.js
+++ src/api/graphql/resolvers/application/query.resolver.js
@@ -1,7 +1,9 @@
 export default {
   Query: {
     applications(_parent, _args, { dataSources }) {
-      return dataSources.applications.findAll();
+      return dataSources.applications.findAll({
+        order: [["name", "ASC"]],
+      });
     },
     getApplicationById(_parent, { id }, { dataSources }) {
       return dataSources.applications.findByPk(id);
