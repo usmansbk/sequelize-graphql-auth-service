2d0c2fab8a398404171b0e91a7a6510deccec126
usmansbk <usmansbk@gmail.com>
2022-06-27 22:24:10 +0100

    set logger default name

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,5 +1,5 @@
 import bunyan from "bunyan";
 
-const log = bunyan.createLogger({ name: process.env.APP_NAME });
+const log = bunyan.createLogger({ name: process.env.APP_NAME || "logger" });
 
 export default log;
