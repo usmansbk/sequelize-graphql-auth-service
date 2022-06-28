b77a46aed2818577a9373537fac68a8e144e7721
usmansbk <usmansbk@gmail.com>
2022-06-28 18:15:07 +0100

    build

--- tests/factories/permission.js
+++ tests/factories/permission.js
@@ -3,7 +3,7 @@ import { faker } from "@faker-js/faker";
 const define = {
   modelName: "Permission",
   attributes: () => ({
-    scope: faker.unique(faker.name.firstName),
+    scope: faker.unique(faker.random.word),
     description: "A short description",
   }),
   associations: {
