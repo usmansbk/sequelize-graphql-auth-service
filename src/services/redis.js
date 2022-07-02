555aa9967ba4f2babf483cdb3b6ad2b8de9e3462
usmansbk <usmansbk@gmail.com>
2022-07-02 03:08:29 +0100

    fix redis production

--- src/services/redis.js
+++ src/services/redis.js
@@ -2,7 +2,11 @@ import Redis from "ioredis";
 
 const createClient = () => {
   if (process.env.REDIS_URL) {
-    return new Redis(process.env.REDIS_URL);
+    return new Redis(process.env.REDIS_URL, {
+      tls: {
+        rejectUnauthorized: false,
+      },
+    });
   }
   return new Redis({
     port: process.env.REDIS_PORT,
