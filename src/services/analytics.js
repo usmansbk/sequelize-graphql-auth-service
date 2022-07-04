544c9904d2872528547b812892c014d35576fe3d
usmansbk <usmansbk@gmail.com>
2022-07-04 23:22:50 +0100

    nit

--- src/services/analytics.js
+++ src/services/analytics.js
@@ -1,9 +1,10 @@
 import Analytics from "analytics-node";
 
 const { SEGMENT_WRITE_KEY, NODE_ENV } = process.env;
+
 const analytics = new Analytics(SEGMENT_WRITE_KEY, {
   enable: NODE_ENV !== "test",
-  flushAt: 1,
+  flushAt: NODE_ENV === "production" ? 20 : 1,
 });
 
 export default analytics;
