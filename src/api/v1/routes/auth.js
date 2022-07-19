c6f9e53eb8c22bc74815a264c9946eee72379a24
usmansbk <usmansbk@gmail.com>
2022-07-19 14:36:33 +0100

    nit

--- src/api/v1/routes/auth.js
+++ src/api/v1/routes/auth.js
@@ -5,6 +5,5 @@ import AuthController from "../controllers/auth";
 const router = express.Router();
 
 router.post("/refresh_token", timeout("45s"), AuthController.refreshToken);
-router.get("/.well-known/jwks", timeout("60s"), AuthController.getJWKS);
 
 export default router;
